2d623743787e97f816199941a2faf720d0918b30
Rob Hoelz <rob@hoelz.ro>
2011-12-19 18:20:11 -0600

    Redirect to /main on root URL
    Quick and dirty fix for lack of navigation on /

--- lib/Act/Dispatcher.pm
+++ lib/Act/Dispatcher.pm
@@ -109,8 +109,20 @@ sub conference_app {
         enable sub {
             my $app = shift;
             sub {
-                for ( $_[0]->{'PATH_INFO'} ) {
-                    if ( s{^/?$}{index.html} || /\.html$/ ) {
+                my ( $env ) = @_;
+
+                for ( $env->{'PATH_INFO'} ) {
+                    if( m{^/$} ) {
+                        my $req = Plack::Request->new($env);
+                        my $uri = $req->uri . 'main';
+
+                        return [
+                            302,
+                            [ Location => $uri ],
+                            [],
+                        ];
+                    }
+                    elsif ( /\.html$/ ) {
                         return $static_app->(@_);
                     }
                     else {
